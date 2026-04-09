7fdf1a4dec2803b715145d4281cb008077d22c4d
Nikao777 <139736216+Nicolas7720@users.noreply.github.com>
2026-04-09 18:37:19 -0300

    Projeto inicial

---
+++ Backend/src/test/java/br/com/trabalho/faculdade/baclend/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trabalho.faculdade.baclend.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
